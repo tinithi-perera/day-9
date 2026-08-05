d52d4f3b99db004a07c703cb3ba57195d6dfcd69
tinithi-perera <tinithiperera@gmail.com>
2026-08-05 23:00:26 +0530

    Add arithmetic functions and update script type in HTML

---
+++ app.js
@@ -0,0 +1,16 @@
+import {sum,divide,multiply,subtract} from './sum.js';
+let result = sum(1, 2);
+console.log(result);
+
+;
+let result2 = divide(4, 2);
+console.log(result2);
+
+
+let result3 = multiply(2, 3);
+console.log(result3);
+
+
+let result4 = subtract(5, 3);
+console.log(result4);
+
